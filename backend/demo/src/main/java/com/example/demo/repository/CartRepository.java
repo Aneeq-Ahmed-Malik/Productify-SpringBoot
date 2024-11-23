fa35f3b7db1b3c1f5e375e49ec42c8eedaa1d379
kalbe-raza <razanaqvi813@gmail.com>
2024-11-23 21:11:48 +0500

    cart operations

--- backend/demo/src/main/java/com/example/demo/repository/CartRepository.java
+++ backend/demo/src/main/java/com/example/demo/repository/CartRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Long>  {
     Optional<Cart> findByUserId(Long userId);
